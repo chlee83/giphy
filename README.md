d5ade76518e3602dfad8394e321e2f91e07be99b
Hobbes <chilee83@gmail.com>
2019-02-27 13:29:28 -0500

    Second Commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # giphy
+
+Click on any button to see 10 gifs of that animal
+You can add a button of an animal you want to see if it isn't there by typing the animal's name in and pressing "Enter" or clicking the Submit button.
+
+Once gifs are loaded, you can click on any gif to animate it. 
+Clicking the gif after it is animated will make the gif a still image.
---
+++ assets/javascript/app.js
@@ -0,0 +1,140 @@
+$(document).ready(function() {
+
+// List of classes for html
+// form-control  buttonsList  images
+// apiKey: T0s8H2m5E8LgKBd12fUn0RY63Wzo7iiT
+
+var animals = ["dog", "cat", "mouse", "lion", "horse", "bird", "cheetah", "squirrel",
+                "skunk", "snake", "giraffe", "tiger", "turtle", "deer", "koala", "zebra",
+                "hamster", "rabbit", "chinchilla", "armadillo", "donkey", "antelope", "monkey",
+                "ferret", "whale", "shark", "bear", "polar bear", "sloth", "gecko", "vulture",
+                "elephant"];
+
+//display animal gifs fuction
+function displayAnimalInfo() {
+
+    $(".images").empty();
+
+    // Grabbing and storing the data-animal property value from the button
+    var getAnimal = $(this).attr("data-animal");
+
+    // Constructing a queryURL using the animal name
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
+    getAnimal + "&api_key=T0s8H2m5E8LgKBd12fUn0RY63Wzo7iiT&limit=10"
+
+    //ajax request from queryURL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(response) {
+
+        console.log(queryURL);
+        console.log(response);
+
+        var results = response.data;
+        for(var i = 0; i < results.length; i++) {
+
+            //create a new div tag
+            var animalDiv = $("<div>");
+            animalDiv.addClass("mx-1 my-3")
+
+            // create a new paragraph tag that holds the results and rating of image
+            var p = $("<p>").text("Rating: " + results[i].rating);
+
+            //create new image
+            var animalImage = $("<img>");
+
+            animalImage.addClass("gif border border-secondary")
+
+            //add attribute of source of image
+            animalImage.attr("src", results[i].images.fixed_height_still.url);
+            animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+            animalImage.attr("data-animate", results[i].images.fixed_height.url);
+            animalImage.attr("data-state", "still")
+
+            //adding paragraph and image tags to the animal div
+            animalDiv.append(p);
+            animalDiv.append(animalImage);
+
+            $(".images").prepend(animalDiv);
+        }
+
+
+    })
+}
+
+//start and stop gif
+function gifRender() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+};
+
+// start by populating some initial buttons
+function renderButtons() {
+
+    //clear all buttons initially
+    $(".buttonsList").empty();
+
+    //for loop to put all the animals in array into buttons
+    for (var i = 0; i < animals.length; i++) {
+
+        //variable a is assigned to a new button jquery
+        var a = $("<button>");
+
+        //class is added to variable a
+        a.addClass("btn btn-outline-info animal");
+
+        //attribute of data-name is added to each animal's name from array
+        a.attr("data-animal", animals[i]);
+
+        //the text of the animals name from array is put on button to show
+        a.text(animals[i]);
+
+        //each new button is appended 
+        $(".buttonsList").append(a);
+    }
+}
+
+// onclick function for each animal button only if not already in array
+$("#addAnimal").on("click", function(event) {
+
+    event.preventDefault();
+
+    //grabs the input from the textbox
+    var newAnimal = $("#inputText").val().trim();
+
+    //only push new animal to array if not already in
+    if (animals.indexOf(newAnimal) === -1 && newAnimal !== "") {
+
+        //add the text from textbox to array
+        animals.push(newAnimal);
+        
+        //call render button function
+        renderButtons();
+    }
+});
+
+// adding an on click listener to all elements with a class of animal
+$(document).on("click", ".animal", displayAnimalInfo);
+
+$(document).on("click", ".gif", gifRender);
+
+renderButtons();
+
+
+
+
+//end of document ready
+});
